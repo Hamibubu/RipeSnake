ffecfdbb14776951874028769d1795570d65c152
zavas400 <79598951+zavas400@users.noreply.github.com>
2023-11-25 14:18:09 -0600

    se ve mas lindo

--- movimiento.c
+++ movimiento.c
@@ -13,14 +13,11 @@ void main()
 unsigned int mask = 0;
 while(1)
 {
-    if(*d_pad_up == 1)
-        led_base -= LED_MATRIX_0_WIDTH;
-    if(*d_pad_do == 1)
-        led_base += LED_MATRIX_0_WIDTH;
-    if(*d_pad_le == 1)
-        led_base -= 1;
-    if(*d_pad_ri == 1)
-        led_base += 1;
-        *led_base = 0xFFD700;
+    if(*d_pad_up == 1) led_base -= LED_MATRIX_0_WIDTH;
+    if(*d_pad_do == 1) led_base += LED_MATRIX_0_WIDTH;
+    if(*d_pad_le == 1) led_base -= 1;
+    if(*d_pad_ri == 1) led_base += 1;
+
+    *led_base = 0xFFD700;
 }
 }
